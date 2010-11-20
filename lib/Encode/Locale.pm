c79b22b2d0d7dc783bf60f607128c5c94cb9d103
Gisle Aas <gisle@aas.no>
2010-11-20 13:02:58 +0100

    Provide a reinit() interface

--- lib/Encode/Locale.pm
+++ lib/Encode/Locale.pm
@@ -18,54 +18,78 @@ our $ENCODING_LOCALE_FS;
 our $ENCODING_CONSOLE_IN;
 our $ENCODING_CONSOLE_OUT;
 
-if ($^O eq "MSWin32") {
-    # If we have the Win32::Console module installed we can ask
-    # it for the code set to use
-    eval {
-	require Win32::Console;
-	my $cp = Win32::Console::InputCP();
-	$ENCODING_CONSOLE_IN = "cp$cp" if $cp;
-	$cp = Win32::Console::OutputCP();
-	$ENCODING_CONSOLE_OUT = "cp$cp" if $cp;
-    };
-    # Invoking the 'chcp' program might also work
-    if (!$ENCODING_CONSOLE_IN && qx(chcp) =~ /^Active code page: (\d+)/) {
-	$ENCODING_CONSOLE_IN = "cp$1";
+sub DEBUG () { 0 }
+
+sub _init {
+    if ($^O eq "MSWin32") {
+	# If we have the Win32::Console module installed we can ask
+	# it for the code set to use
+	eval {
+	    require Win32::Console;
+	    my $cp = Win32::Console::InputCP();
+	    $ENCODING_CONSOLE_IN = "cp$cp" if $cp;
+	    $cp = Win32::Console::OutputCP();
+	    $ENCODING_CONSOLE_OUT = "cp$cp" if $cp;
+	};
+	# Invoking the 'chcp' program might also work
+	if (!$ENCODING_CONSOLE_IN && qx(chcp) =~ /^Active code page: (\d+)/) {
+	    $ENCODING_CONSOLE_IN = "cp$1";
+	}
+    }
+
+    unless ($ENCODING_LOCALE) {
+	eval {
+	    require I18N::Langinfo;
+	    $ENCODING_LOCALE = I18N::Langinfo::langinfo(I18N::Langinfo::CODESET());
+
+	    # Workaround of Encode < v2.25.  The "646" encoding  alias was
+	    # introducted in Encode-2.25, but we don't want to require that version
+	    # quite yet.  Should avoid the CPAN testers failure reported from
+	    # openbsd-4.7/perl-5.10.0 combo.
+	    $ENCODING_LOCALE = "ascii" if $ENCODING_LOCALE eq "646";
+	};
+	$ENCODING_LOCALE ||= $ENCODING_CONSOLE_IN;
+    }
+
+    if ($^O eq "darwin") {
+	$ENCODING_LOCALE_FS ||= "UTF-8";
+    }
+
+    # final fallback
+    $ENCODING_LOCALE ||= $^O eq "MSWin32" ? "cp1252" : "UTF-8";
+    $ENCODING_LOCALE_FS ||= $ENCODING_LOCALE;
+    $ENCODING_CONSOLE_IN ||= $ENCODING_LOCALE;
+    $ENCODING_CONSOLE_OUT ||= $ENCODING_CONSOLE_IN;
+
+    unless (Encode::find_encoding($ENCODING_LOCALE)) {
+	die "The locale codeset ($ENCODING_LOCALE) isn't one that perl can decode, stopped";
     }
 }
 
-unless ($ENCODING_LOCALE) {
-    eval {
-	require I18N::Langinfo;
-	$ENCODING_LOCALE = I18N::Langinfo::langinfo(I18N::Langinfo::CODESET());
-
-	# Workaround of Encode < v2.25.  The "646" encoding  alias was
-	# introducted in Encode-2.25, but we don't want to require that version
-	# quite yet.  Should avoid the CPAN testers failure reported from
-	# openbsd-4.7/perl-5.10.0 combo.
-	$ENCODING_LOCALE = "ascii" if $ENCODING_LOCALE eq "646";
-    };
-    $ENCODING_LOCALE ||= $ENCODING_CONSOLE_IN;
-}
-
-if ($^O eq "darwin") {
-    $ENCODING_LOCALE_FS ||= "UTF-8";
-}
-
-# final fallback
-$ENCODING_LOCALE ||= $^O eq "MSWin32" ? "cp1252" : "UTF-8";
-$ENCODING_LOCALE_FS ||= $ENCODING_LOCALE;
-$ENCODING_CONSOLE_IN ||= $ENCODING_LOCALE;
-$ENCODING_CONSOLE_OUT ||= $ENCODING_CONSOLE_IN;
-
-unless (Encode::find_encoding($ENCODING_LOCALE)) {
-    die "The locale codeset ($ENCODING_LOCALE) isn't one that perl can decode, stopped";
-}
-
+_init();
 Encode::Alias::define_alias(sub {
     no strict 'refs';
     return ${"ENCODING_" . uc(shift)};
 }, "locale");
+
+sub _flush_aliases {
+    no strict 'refs';
+    for my $a (keys %Encode::Alias::Alias) {
+	if (defined ${"ENCODING_" . uc($a)}) {
+	    delete $Encode::Alias::Alias{$a};
+	    warn "Flushed alias cache for $a" if DEBUG;
+	}
+    }
+}
+
+sub reinit {
+    $ENCODING_LOCALE = shift;
+    $ENCODING_LOCALE_FS = shift;
+    $ENCODING_CONSOLE_IN = $ENCODING_LOCALE;
+    $ENCODING_CONSOLE_OUT = $ENCODING_LOCALE;
+    _init();
+    _flush_aliases();
+}
 
 sub decode_argv {
     die if defined wantarray;
@@ -123,7 +147,7 @@ In addition the following functions and variables are provided:
 
 =over
 
-=item decode_argv()
+=item decode_argv( )
 
 =item decode_argv( Encode::FB_CROAK )
 
@@ -148,6 +172,18 @@ environment variable named $uni_key.
 
 The returned value will have the characters that can't be decoded replaced by
 "\x{FFFD}", the Unicode replacement character.
+
+=item reinit( )
+
+=item reinit( $encoding )
+
+Reinitialize the encodings from the locale.  You want to call this function if
+changed anything in the environment that might influence the locale.
+
+This function will croak if the determined encoding isn't recognized by
+the Encode module.
+
+With argument force $ENCODING_... variables to set to the given value.
 
 =item $ENCODING_LOCALE
 
--- t/env.t
+++ t/env.t
@@ -2,7 +2,7 @@
 
 use strict;
 use Test;
-plan tests => 7;
+plan tests => 13;
 
 use Encode::Locale qw(env);
 
@@ -14,3 +14,12 @@ ok($ENV{foo}, "baz");
 ok(env("foo", undef), "baz");
 ok(env("foo"), undef);
 ok(!exists $ENV{foo});
+
+Encode::Locale::reinit("cp1252");
+$ENV{"m\xf6ney"} = "\x80uro";
+ok(env("m\xf6ney", "\x{20AC}"), "\x{20AC}uro");
+ok(env("m\xf6ney"), "\x{20AC}");
+ok($ENV{"m\xf6ney"}, "\x80");
+ok(env("\x{20AC}", 1), undef);
+ok(env("\x{20AC}"), 1);
+ok($ENV{"\x80"}, 1);
